8ea406407e744a4f7ce377c82c90fd30455b226f
ddz <ddzyan@163.com>
2019-11-04 12:35:44 +0800

    添加代码注释

--- koa-analysis/koa-compose.js
+++ koa-analysis/koa-compose.js
@@ -18,6 +18,9 @@ function compose(md) {
   return function(context, next) {
     let index = -1;
     return dispatch(0);
+    /**
+     * @return 返回结果为 Promise对象
+     */
     function dispatch(i) {
       if (i <= index)
         return Promise.reject(new Error("next() called multiple times"));
@@ -26,6 +29,7 @@ function compose(md) {
       if (i === md.length) fn = next;
       if (!fn) return Promise.resolve();
       try {
+        // 执行中间件，并且传入next() 函数，用于执行下一个中间
         return Promise.resolve(fn(context, dispatch.bind(null, i + 1)));
       } catch (error) {
         return Promise.reject(error);
