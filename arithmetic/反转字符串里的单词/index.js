b5e61ae34e1c3793dbb371abc0d17604cf3081fb
ddz <ddzyan@163.com>
2020-04-10 13:05:32 +0800

    更新算法题

---
+++ arithmetic/反转字符串里的单词/index.js
@@ -0,0 +1,15 @@
+const asserts = require('assert');
+
+const reverseWords = (s) => {
+  const oldStrArr = s.split(' ');
+  // 去除多余空格
+  const oldStrArrNotTrim = oldStrArr.filter((item) => item.length > 0);
+  // 反转数组
+  oldStrArrNotTrim.reverse();
+  // 返回拼接字符串
+  return oldStrArrNotTrim.join(' ');
+};
+
+const newStr = reverseWords('  hello world!  ');
+console.log('转换结果', newStr);
+asserts.equal(newStr, 'world! hello', '结果错误');
