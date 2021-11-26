a1760fc403d69393a05d7fbc6bdbd71276b2495e
dingdongzhao <ddzyan@163.com>
2021-11-26 17:30:05 +0800

    🌈 style(设计模式/单例.js): 代码格式优化

--- 设计模式/单例.js
+++ 设计模式/单例.js
@@ -6,24 +6,24 @@ class App {
   constructor() {
     // 如果已存在对应的实例
     if (typeof App.instance === 'object') {
-      return App.instance
+      return App.instance;
     }
 
     // 缓存
-    App.instance = this
-    return this
+    App.instance = this;
+    return this;
   }
 
   set name(name) {
-    this._name = name
+    this._name = name;
   }
 
   get name() {
-    return `这是${this._name}`
+    return `这是${this._name}`;
   }
 }
 
-const app1 = new App()
-const app2 = new App()
+const app1 = new App();
+const app2 = new App();
 
-console.log(app1 === app2)
+console.log(app1 === app2);
